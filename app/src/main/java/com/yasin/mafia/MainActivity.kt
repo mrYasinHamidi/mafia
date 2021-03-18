0f555498cda2d720c1fa5980d985772884763b25
yasin <yasinhamidi50@gmail.com>
2021-03-18 10:38:14 +0330

    submit project to github

--- app/src/main/java/com/yasin/mafia/MainActivity.kt
+++ app/src/main/java/com/yasin/mafia/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.yasin.mafia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.yasin.mafia.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
+
+
+
+
 }
